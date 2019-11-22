ea3a52734f6ab52d069c71e92ba653fc33927350
Axten <gigdamato@gmail.com>
2019-11-22 12:39:28 +0100

    Notification button test
    set string to "Next!"
    added method notifyme and createChannel in mainActivity.java
    set API from 24 to 26 to simplify most method inputs
    added words to android studio dictionary

--- NextApp/wear/src/main/java/com/example/next_app/MainActivity.java
+++ NextApp/wear/src/main/java/com/example/next_app/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.next_app;
 
+import android.app.Notification;
 import android.os.Bundle;
+import android.app.NotificationManager;
+import android.app.NotificationChannel;
+import android.content.Context;
 import android.support.wearable.activity.WearableActivity;
 import android.widget.TextView;
+import android.view.View;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class MainActivity extends WearableActivity {
 
@@ -13,9 +21,40 @@ public class MainActivity extends WearableActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        mTextView = findViewById(R.id.text);
 
         // Enables Always-on
         setAmbientEnabled();
+    }
+
+    public void notifyme(View view){
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, createChannel(mNotificationManager).getId());
+        builder.setContentTitle("titolo della notifica");
+        builder.setContentText("contenuto della notifica");
+        builder.setSmallIcon(R.drawable.common_google_signin_btn_icon_dark);
+
+        Notification notification = builder.build();
+
+        NotificationManagerCompat nmc = NotificationManagerCompat.from(this);
+        nmc.notify(1,notification);
+    }
+
+    private NotificationChannel createChannel(NotificationManager nManager){
+        String id ="my_channel_01";
+        CharSequence name =  "channel-name";
+        int importance = nManager.IMPORTANCE_DEFAULT;
+
+        NotificationChannel nChannel = new NotificationChannel(id, name,importance);
+
+        String description = "description";
+        nChannel.setDescription(description);
+        nChannel.enableLights(true);
+        nChannel.enableVibration(true);
+
+        nManager.createNotificationChannel(nChannel);
+
+        return nChannel;
     }
 }
