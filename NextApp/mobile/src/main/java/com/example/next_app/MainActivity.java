c5f0352e54446a081ce3785020436e2af596985f
Fabrizio <campo.fabrizio9@gmail.com>
2019-11-29 17:59:40 +0100

    Mobile interface 2nd step

--- NextApp/mobile/src/main/java/com/example/next_app/MainActivity.java
+++ NextApp/mobile/src/main/java/com/example/next_app/MainActivity.java
@@ -6,12 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.wearable.Wearable;
 
 public class MainActivity extends AppCompatActivity {
-    private Button b;
+    private ImageButton b;
 
 
     GoogleApiClient gAPIclient = null;
@@ -20,11 +21,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b=(Button) findViewById(R.id.button);
+        b=(ImageButton) findViewById(R.id.imageButton);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i=new Intent(MainActivity.this,SettingsActivity.class);
+                Intent i=new Intent(MainActivity.this,Main2Activity.class);
                 startActivity(i);
             }
         });
