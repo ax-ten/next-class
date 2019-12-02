e3808ae18550df41ec1d6c6d4507559dfaec208f
Axten <gigdamato@gmail.com>
2019-12-02 20:08:10 +0100

    Phone main activity merge
    removal of datatask

--- NextApp/mobile/src/main/java/com/example/next_app/Phone_MainActivity.java
+++ NextApp/mobile/src/main/java/com/example/next_app/Phone_MainActivity.java
@@ -1,18 +1,18 @@
 package com.example.next_app;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.XmlResourceParser;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
+import android.widget.ImageButton;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -21,19 +21,51 @@ import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.NodeApi;
 import com.google.android.gms.wearable.Wearable;
 
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
-public class PhoneActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-    private static final String TAG = "PhoneActivity";
+public class Phone_MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private static final String TAG = "Phone_MainActivity";
     public static final String CONFIG_START = "config/start";
     public static final String CONFIG_STOP= "config/stop";
-
+    private ImageButton b;
     GoogleApiClient mGoogleApiClient;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b=(ImageButton) findViewById(R.id.imageButton);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Phone_MainActivity.this,Main2Activity.class);
+                startActivity(i);
+            }
+        });
+
+        /**
+         if(null == mGoogleApiClient) {
+         mGoogleApiClient = new GoogleApiClient.Builder(this)
+         .addApi(Wearable.API)
+         .addConnectionCallbacks(this)
+         .addOnConnectionFailedListener(this)
+         .build();
+         Log.v(TAG, "GoogleApiClient created");
+         }
+
+         if(!mGoogleApiClient.isConnected()){
+         mGoogleApiClient.connect();
+         Log.v(TAG, "Connecting to GoogleApiClient..");
+         }
+         */
+
+        //startService(new Intent(this, PhoneService.class));
+        //todo riattivare lo start del service
+
+        GoogleApiClient.Builder builder = new GoogleApiClient.Builder(this);
+        builder.addApi(Wearable.API);
+    }
 
     class SendActivityPhoneMessage extends Thread {
         String path;
@@ -92,53 +124,19 @@ public class PhoneActivity extends Activity implements GoogleApiClient.Connectio
 
         return nChannel;
     }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phone);
-
-        /**
-        if(null == mGoogleApiClient) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addApi(Wearable.API)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .build();
-            Log.v(TAG, "GoogleApiClient created");
-        }
-
-        if(!mGoogleApiClient.isConnected()){
-            mGoogleApiClient.connect();
-            Log.v(TAG, "Connecting to GoogleApiClient..");
-        }
-        */
-
-        //startService(new Intent(this, PhoneService.class));
-        //todo riattivare lo start del service
-
-
-        try {
-            new Stub().readFeed(getAssets().open("stubtest.xml"));
-        } catch (XmlPullParserException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     @Override protected void onStart() {
         super.onStart();
-       //Log.v(TAG, "onStart called");
+        //Log.v(TAG, "onStart called");
     }
     @Override public void onConnectionSuspended(int cause)
-        {Log.v(TAG,"onConnectionSuspended called");}
+    {Log.v(TAG,"onConnectionSuspended called");}
     @Override public void onConnectionFailed(ConnectionResult connectionResult)
-        {Log.v(TAG,"onConnectionFailed called");}
+    {Log.v(TAG,"onConnectionFailed called");}
     @Override public void onConnected(Bundle connectionHint)
-        {Log.v(TAG,"onConnected called");}
+    {Log.v(TAG,"onConnected called");}
 
-    public void configStart(View view){new SendActivityPhoneMessage(CONFIG_START,"").start();}
-    public void configStop(View view){new SendActivityPhoneMessage(CONFIG_STOP,"").start();}
+    public void configStart(View view){new Phone_MainActivity.SendActivityPhoneMessage(CONFIG_START,"").start();}
+    public void configStop(View view){new Phone_MainActivity.SendActivityPhoneMessage(CONFIG_STOP,"").start();}
+
 
 }
