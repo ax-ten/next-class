b35a16d07fd13a3d7a1474cc8d20e95ae530892d
Axten <gigdamato@gmail.com>
2019-12-03 00:57:11 +0100

    Remade parsing
    moved parsing method from Stub.java to phone's main activity

--- NextApp/mobile/src/main/java/com/example/next_app/Phone_MainActivity.java
+++ NextApp/mobile/src/main/java/com/example/next_app/Phone_MainActivity.java
@@ -1,18 +1,17 @@
 package com.example.next_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
-import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -21,20 +20,31 @@ import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.NodeApi;
 import com.google.android.gms.wearable.Wearable;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.LinkedList;
 import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class Phone_MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     private static final String TAG = "Phone_MainActivity";
     public static final String CONFIG_START = "config/start";
     public static final String CONFIG_STOP= "config/stop";
-    private ImageButton b;
     GoogleApiClient mGoogleApiClient;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.phone_main_activity);
 
         /**
          if(null == mGoogleApiClient) {
@@ -55,10 +65,24 @@ public class Phone_MainActivity extends AppCompatActivity implements GoogleApiCl
         //startService(new Intent(this, PhoneService.class));
         //todo riattivare lo start del service
 
-        GoogleApiClient.Builder builder = new GoogleApiClient.Builder(this);
-        builder.addApi(Wearable.API);
+        //GoogleApiClient.Builder builder = new GoogleApiClient.Builder(this);
+        //builder.addApi(Wearable.API);
+
+        //PARSE
+        AssetManager am = this.getAssets();
+        InputStream istream = null;
+        try {
+            Log.v(TAG, "caricamento file");
+            istream = am.open("schedule_stubs.xml");
+            Log.v(TAG, "parse iniziato");
+            LinkedList<Stub> stubList = parseXML(istream);
+            Log.v(TAG, "parse terminato");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    //NOTIFICATIONS
     class SendActivityPhoneMessage extends Thread {
         String path;
         String message;
@@ -126,6 +150,81 @@ public class Phone_MainActivity extends AppCompatActivity implements GoogleApiCl
     {Log.v(TAG,"onConnectionFailed called");}
     @Override public void onConnected(Bundle connectionHint)
     {Log.v(TAG,"onConnected called");}
+
+    //BUTTONS
+    public void onClick_addNew(View view){
+        Log.v(TAG,"addnew")
+        //todo
+    }
+
+    public void onClick_settings(View view){
+        //todo
+    }
+
+    public void onClick_parse(View view){
+        AssetManager am = this.getAssets();
+        InputStream istream = null;
+        try {
+            Log.v(TAG, "caricamento file");
+            istream = am.open("schedule_stubs.xml");
+            Log.v(TAG, "parse iniziato");
+            LinkedList<Stub> stubList = parseXML(istream);
+            Log.v(TAG, "parse terminato");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //PARSING
+    public  LinkedList<Stub> parseXML(InputStream istream){
+        LinkedList<Stub> stubList = new LinkedList<>();
+        try{
+            DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = builderFactory.newDocumentBuilder();
+            Document doc = docBuilder.parse(istream);
+
+            NodeList courses = doc.getElementsByTagName("class");
+            for(int i =0;i<courses.getLength();i++){
+                Element elm = (Element)courses.item(i);
+                Stub stub = new Stub(
+                        /**Integer.parseInt(getNodeValue("year",elm)),
+                        Integer.parseInt(getNodeValue("day",elm)),
+                        getNodeValue("name",elm),
+                        getNodeValue("teacher",elm),
+                        getNodeValue("room",elm),
+                        Double.parseDouble(getNodeValue("startTime",elm)),
+                        Double.parseDouble(getNodeValue("endTime",elm))
+                         */
+                        i,i,getNodeValue("name",elm),null, null,i,i
+                );
+                stubList.add(stub);
+
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+        return stubList;
+    }
+    protected String getNodeValue(String tag, Element element) {
+        NodeList nodeList = element.getElementsByTagName(tag);
+        org.w3c.dom.Node node = nodeList.item(0);
+        if(node!=null) {
+            if (node.hasChildNodes()) {
+                org.w3c.dom.Node child = node.getFirstChild();
+                while (child != null) {
+                    if (child.getNodeType() == org.w3c.dom.Node.TEXT_NODE) {
+                        return child.getNodeValue();
+                    }
+                }
+            }
+        }
+        return "";
+    }
 
     public void configStart(View view){new Phone_MainActivity.SendActivityPhoneMessage(CONFIG_START,"").start();}
     public void configStop(View view){new Phone_MainActivity.SendActivityPhoneMessage(CONFIG_STOP,"").start();}
--- NextApp/mobile/src/main/java/com/example/next_app/Stub.java
+++ NextApp/mobile/src/main/java/com/example/next_app/Stub.java
@@ -2,97 +2,25 @@ package com.example.next_app;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.content.res.XmlResourceParser;
-import android.icu.util.Calendar;
-import android.util.Log;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 public class Stub {
-    private int id,year, day;
-    private String courseName, teacherName, room;
-    private double startTime, endTime;
+    public int year, day;
+    public String courseName, teacherName, room;
+    public double startTime, endTime;
 
     //Constructors
-    public Stub(int y, int d, int id){
-        this.id = id;
+    public Stub(int y, int d, String cN, String tN, String r, double sT, double eT){
         this.year = y;
         this.day = d;
-
-
-        /** todo implement these
-        this.courseName = ;
-        this.startTime = ;
-        this.endTime = ;
-        this.room = ;
-        this.teacherName = ;
-         */
-
-
+        this.courseName = cN;
+        this.teacherName = tN;
+        this.room = r;
+        this.startTime = sT;
+        this.endTime = eT;
     }
-    public Stub(){}
-
-
-    //Public methods
-    public List readFeed(InputStream is) throws XmlPullParserException, IOException{
-        List entries = new ArrayList();
-        XmlPullParserFactory parserFactory;
-
-        try{
-           parserFactory  = XmlPullParserFactory.newInstance();
-           XmlPullParser parser = parserFactory.newPullParser();
-           parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-           parser.setInput(is,null);
-
-           processParsing(parser);
-        } catch (XmlPullParserException e) {
-            e.printStackTrace();
-        }catch (IOException e) {
-        }
-
-        return entries;
-    }
-
-
 
     //Other methods
-    private void processParsing(XmlPullParser parser) throws  IOException, XmlPullParserException{
-        ArrayList<Stub> stubs = new ArrayList<>();
-        int eventType = parser.getEventType();
-        Stub currentStub = null;
-
-        while (eventType != XmlPullParser.END_DOCUMENT){
-            String eltName = null;
-
-            switch (eventType){
-                case XmlPullParser.START_TAG:
-                    eltName = parser.getName();
-                    if ("class".equals(eltName)){
-                        currentStub = new Stub();
-                    }
-            }
-        }
-    }
-
-    private String[] getDays(Context context){
+    private final String[] getDays(Context context){
         Resources resources = context.getResources();
         String[] days = new String[]
                 {resources.getString(R.string.monday),
