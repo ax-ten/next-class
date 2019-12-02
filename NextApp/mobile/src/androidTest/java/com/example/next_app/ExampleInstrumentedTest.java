d0c96427f20436549eabb1a983587fed46d16aa1
Axten <gigdamato@gmail.com>
2019-12-03 00:56:11 +0100

    Removed methods from testing app

--- NextApp/mobile/src/androidTest/java/com/example/next_app/ExampleInstrumentedTest.java
+++ NextApp/mobile/src/androidTest/java/com/example/next_app/ExampleInstrumentedTest.java
@@ -1,22 +1,14 @@
 package com.example.next_app;
 
 import android.content.Context;
-import android.content.res.XmlResourceParser;
-import android.util.Log;
 
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -31,26 +23,9 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        try {
-            readFeed(appContext.getResources().getXml(R.xml.schedule_stubs));
-        } catch (XmlPullParserException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
         assertEquals("com.example.next_app", appContext.getPackageName());
     }
 
-    private List readFeed(XmlResourceParser parser) throws XmlPullParserException, IOException {
-        List entries = new ArrayList();
-        int xmlTAG = parser.getEventType();
-        Log.d("test","fin qui ha funzionato sir");
-        while (xmlTAG != XmlPullParser.END_TAG){
-            String name = parser.getName();
-            Log.d("test", parser.getText() );
-            parser.nextTag();
-        }
-        return entries;
-    }
+
 }
