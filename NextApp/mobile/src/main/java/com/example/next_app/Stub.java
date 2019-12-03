b4f002f9086d745230146b66d75eae56972b038a
Axten <gigdamato@gmail.com>
2019-12-03 15:02:44 +0100

    Added setters to Stub
    changed attributes from public to private

--- NextApp/mobile/src/main/java/com/example/next_app/Stub.java
+++ NextApp/mobile/src/main/java/com/example/next_app/Stub.java
@@ -4,9 +4,9 @@ import android.content.Context;
 import android.content.res.Resources;
 
 public class Stub {
-    public int year, day;
-    public String courseName, teacherName, room;
-    public double startTime, endTime;
+    private int year, day;
+    private String courseName, teacherName, room;
+    private double startTime, endTime;
 
     //Constructors
     public Stub(int y, int d, String cN, String tN, String r, double sT, double eT){
@@ -18,9 +18,42 @@ public class Stub {
         this.startTime = sT;
         this.endTime = eT;
     }
+
+
     public Stub(){
         //empty
     }
+
+
+    //Setters
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public void setStartTime(double startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(double endTime) {
+        this.endTime = endTime;
+    }
+
 
     //Other methods
     private final String[] getDays(Context context){
