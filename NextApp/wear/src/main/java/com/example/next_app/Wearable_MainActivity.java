47df1b1337ecefb79f4d2c28ff24b71428574b3e
Axten <gigdamato@gmail.com>
2019-12-03 18:23:10 +0100

    Minor fixes and removed unused methods

--- NextApp/wear/src/main/java/com/example/next_app/Wearable_MainActivity.java
+++ NextApp/wear/src/main/java/com/example/next_app/Wearable_MainActivity.java
@@ -7,9 +7,10 @@ import android.app.NotificationChannel;
 import android.content.Context;
 import android.support.v4.os.IResultReceiver;
 import android.support.wearable.activity.WearableActivity;
-import android.util.Log;
 import android.widget.TextView;
 import android.view.View;
+
+import 
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
@@ -18,9 +19,6 @@ import androidx.core.app.NotificationManagerCompat;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.wearable.DataApi;
-import com.google.android.gms.wearable.DataEvent;
-import com.google.android.gms.wearable.DataEventBuffer;
-import com.google.android.gms.wearable.DataMapItem;
 import com.google.android.gms.wearable.Wearable;
 
 import java.util.LinkedList;
@@ -47,7 +45,7 @@ public class Wearable_MainActivity extends WearableActivity implements GoogleApi
         private onRefresh(View view){
             TextView teacherName= findViewById(R.id.teacher_text);
             teacherName.setText(stub.getTeacherName());
-            TextView classname=findViewById(R.id.classname_text)
+            TextView classname=findViewById(R.id.classname_text);
         }
     }
 
@@ -70,9 +68,6 @@ public class Wearable_MainActivity extends WearableActivity implements GoogleApi
         //todo getstub
     }
 
-    public void open_menu(View view){
-        //todo
-    }
 
     private NotificationChannel createChannel(NotificationManager nManager){
         String id ="my_channel_01";
@@ -93,40 +88,15 @@ public class Wearable_MainActivity extends WearableActivity implements GoogleApi
 
     @Override
     public void onConnected(Bundle bundle) {
-
         Wearable.DataApi.addListener(googleApiClient, (DataApi.DataListener) this);
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        //todo
     }
-
-    public void onDataChanged(DataEventBuffer dataEvents) {
-
-        for (DataEvent event: dataEvents) {
-
-            Log.d("[DEBUG] DeviceService - onDataChanged",
-                    "Event received: " + event.getDataItem().getUri());
-
-            String eventUri = event.getDataItem().getUri().toString();
-
-            if (eventUri.contains ("/myapp/myevent")) {
-
-                DataMapItem dataItem = DataMapItem.fromDataItem (event.getDataItem());
-                String[] data = dataItem.getDataMap().getStringArray("contents");
-
-                Log.d("[DEBUG] DeviceService - onDataChanged", "Sending timeline to the listener");
-
-                //myListener.onDataReceived(data);
-            }
-        }
-    }
-
-
-
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        //todo
     }
 }
