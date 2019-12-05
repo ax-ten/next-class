f5b742b70c8968b13ac84568ddb0be38dacc8cf4
Axten <gigdamato@gmail.com>
2019-12-05 16:42:57 +0100

    Changed Adapter from array to custom

--- NextApp/mobile/src/main/java/com/example/next_app/ScheduleListFragment.java
+++ NextApp/mobile/src/main/java/com/example/next_app/ScheduleListFragment.java
@@ -1,20 +1,19 @@
 package com.example.next_app;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.ListFragment;
 
-public class ScheduleListFragment extends ListFragment implements AdapterView.OnItemLongClickListener, AdapterView.OnItemClickListener, View.OnLongClickListener {
+public class ScheduleListFragment extends ListFragment implements AdapterView.OnItemClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
@@ -26,17 +25,23 @@ public class ScheduleListFragment extends ListFragment implements AdapterView.On
     public void onActivityCreated(@Nullable Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
 
-        ArrayAdapter arrayAdapter = ArrayAdapter.createFromResource(
-                getActivity(),R.array.filenamelist, android.R.layout.simple_list_item_1);
-        setListAdapter(arrayAdapter);
-        getListView().setOnItemClickListener(this);
-        getListView().setOnLongClickListener(this);
+        CustomListAdapter cListAdapter = new CustomListAdapter(
+                getActivity(),getResources().getStringArray(R.array.filenamelist));
+
+        setListAdapter(cListAdapter);
+
+
+
     }
 
+
+
     @Override
-    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(getContext(), "Long click yet to implement", Toast.LENGTH_SHORT).show();
-        Button button = (Button) parent.getItemAtPosition(position);
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getContext(), "Item click yet to implement", Toast.LENGTH_SHORT).show();
+        String s = (String) parent.getItemAtPosition(position);
+        Toast.makeText(getContext(), s, Toast.LENGTH_SHORT).show();
+        /**Button button = (Button) parent.getItemAtPosition(position);
         PopupMenu popupMenu = new PopupMenu(getContext(), button );
         popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
 
@@ -48,18 +53,44 @@ public class ScheduleListFragment extends ListFragment implements AdapterView.On
         });
 
         popupMenu.show();
-        return true;
+         */
     }
+
+}
+
+class CustomListAdapter extends ArrayAdapter {
+    //to reference the Activity
+    private Activity context;
+
+    //to store the list of countries
+    private  String[] infoArray;
+
+    private String[] nameArray;
+
+
+    //CONSTRUCTOR
+    public CustomListAdapter(Activity context, String[] nameArrayParam){
+
+        super(context,R.layout.schedule_list_item , nameArrayParam);
+        this.context=context;
+        this.nameArray = nameArrayParam;
+    }
+
+
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(getContext(), "Item click yet to implement", Toast.LENGTH_SHORT).show();
-        //oggetto cliccato: parent.getItemAtPosition(position)
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater= context.getLayoutInflater();
+        View itemView=inflater.inflate(R.layout.schedule_list_item, null,true);
+
+        //this code gets references to objects in the listview_row.xml file
+        TextView nameTextField = (TextView) itemView.findViewById(R.id.filename);
+
+        Button button = (Button)
+        //this code sets the values of the objects to values from the arrays
+        nameTextField.setText(nameArray[position]);
+
+        return itemView;
     }
 
-
-    @Override
-    public boolean onLongClick(View v) {
-        return false;
-    }
 }
