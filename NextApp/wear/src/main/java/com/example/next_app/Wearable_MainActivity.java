15bf8fafbca22491fa2dc76b47b1016b6304fe43
Axten <gigdamato@gmail.com>
2019-12-08 16:06:52 +0100

    Imported libraries in wearable

--- NextApp/wear/src/main/java/com/example/next_app/Wearable_MainActivity.java
+++ NextApp/wear/src/main/java/com/example/next_app/Wearable_MainActivity.java
@@ -3,9 +3,14 @@ package com.example.next_app;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
