780c00cba68fde66f9ebe2e577e32b6e56cc9611
Axten <gigdamato@gmail.com>
2019-12-10 12:54:47 +0100

    ScheduleList fragment Overhaul
    -added libraries
    -set path folder
    -now lists files in that folder
    -returns the associated file for each item

--- NextApp/mobile/src/main/java/com/example/next_app/Phone_Fragment_ScheduleList.java
+++ NextApp/mobile/src/main/java/com/example/next_app/Phone_Fragment_ScheduleList.java
@@ -1,5 +1,6 @@
 package com.example.next_app;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -15,66 +16,96 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.ListFragment;
 
-public class Phone_Fragment_ScheduleList extends ListFragment{
+import com.poliba.mylibrary.Schedule;
 
-    //TODO deve accedere direttamente ad una cartella di file xml nel dispositivo e listare questi file
+import java.io.File;
+import java.util.Objects;
+
+import static com.example.next_app.R.layout.phone_item_schedule;
+
+public class Phone_Fragment_ScheduleList extends ListFragment{
+    private File folder;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-       View view = inflater.inflate(R.layout.phone_fragment_schedulelist, container, false);
-       return view;
+        String path = Objects.requireNonNull(getContext()).getFilesDir() + "next";
+        if (folder == null)
+            folder = new File(path);
+        return inflater.inflate(R.layout.phone_fragment_schedulelist, container, false);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
+        String[] filenames;
+        filenames = getFilenames();
+        if (filenames != null) {
+            CustomListAdapter cListAdapter = new CustomListAdapter(
+                    getActivity(), getFilenames());
+            setListAdapter(cListAdapter);
+        }
+    }
 
-        CustomListAdapter cListAdapter = new CustomListAdapter(
-                getActivity(),getResources().getStringArray(R.array.filenamelist));
+    private String[] getFilenames(){
+        File[] files = folder.listFiles();
+        if (files == null)
+            return null;
 
-        setListAdapter(cListAdapter);
+        String[] filenames = new String[files.length];
+        for (int i=0; i< files.length; i++){
+            filenames[i] = files[i].getName();
+        }
+
+        return filenames;
+    }
+
+    class CustomListAdapter extends ArrayAdapter {
+        private Activity context;
+        private String[] nameArray;
+
+        CustomListAdapter(Activity context, String[] nameArray){
+            super(context, phone_item_schedule, nameArray);
+            this.context=context;
+            this.nameArray = nameArray;
+        }
+
+        @SuppressLint({"ViewHolder","InflateParams"})
+        @Override
+        public View getView(int position, View view, ViewGroup parent) {
+            TextView scheduleNameField;
+            ImageButton optionsButton;
+            final Schedule schedule = new Schedule(Objects.requireNonNull(folder.listFiles())[position]);
+
+            LayoutInflater inflater= context.getLayoutInflater();
+              View itemView=inflater.inflate(phone_item_schedule, null,true);
+
+            scheduleNameField = itemView.findViewById(R.id.filename);
+            scheduleNameField . setText(nameArray[position]);
+            scheduleNameField . setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ((Phone_Activity_Main) Objects.requireNonNull(getActivity())).setCurrentSchedule(schedule);
+                    Toast.makeText(getContext(), "Set as current schedule", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            optionsButton = itemView.findViewById(R.id.popupmenuOpener);
+            PopupMenu popupMenu = new PopupMenu(getContext(), optionsButton );
+            popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    Toast.makeText(getContext(), "Yet to implement", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+            });
+            popupMenu.show();
+
+            return itemView;
+        }
+
     }
 }
 
-class CustomListAdapter extends ArrayAdapter {
-    private Activity context;
-    private String[] nameArray;
 
-    public CustomListAdapter(Activity context, String[] nameArray){
-
-        super(context,R.layout.phone_item_schedule, nameArray);
-        this.context=context;
-        this.nameArray = nameArray;
-    }
-
-
-
-    @Override
-    public View getView(int position, View view, ViewGroup parent) {
-        TextView scheduleNameField;
-        ImageButton optionsButton;
-
-        LayoutInflater inflater= context.getLayoutInflater();
-        View itemView=inflater.inflate(R.layout.phone_item_schedule, null,true);
-
-        scheduleNameField = itemView.findViewById(R.id.filename);
-        scheduleNameField . setText(nameArray[position]);
-
-        optionsButton = itemView.findViewById(R.id.popupmenuOpener);
-        PopupMenu popupMenu = new PopupMenu(getContext(), optionsButton );
-        popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
-
-        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                Toast.makeText(getContext(), "Yet to implement", Toast.LENGTH_SHORT).show();
-                return true;
-            }
-        });
-
-        popupMenu.show();
-
-
-        return itemView;
-    }
-
-}
