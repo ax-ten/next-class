8aaefec7e6d970f6a31539017d9b9814bbf6a67f
Axten <gigdamato@gmail.com>
2019-12-10 20:38:34 +0100

    Updated AddNew
    - name is now required to create file
    - button now redirects on main activity

--- NextApp/mobile/src/main/java/com/example/next_app/Phone_Activity_AddNewSchedule.java
+++ NextApp/mobile/src/main/java/com/example/next_app/Phone_Activity_AddNewSchedule.java
@@ -1,17 +1,15 @@
 package com.example.next_app;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.LinearLayoutCompat;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.gms.common.util.IOUtils;
 
@@ -20,9 +18,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URL;
-import java.net.URLConnection;
 
 public class Phone_Activity_AddNewSchedule extends AppCompatActivity {
     private String TAG = "testing";
@@ -31,30 +27,41 @@ public class Phone_Activity_AddNewSchedule extends AppCompatActivity {
     final String pathName = getApplicationContext().getFilesDir() + "next";
     final InputStream tempResource = getResources().openRawResource(R.raw.schedule_stubs);
 
-    //TODO implementare priorità di inserimento (cdl->anni, nome)
+    //TODO implementare priorità di inserimento (cdl->anni)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.phone_activity_addnewschedule);
 
+        final TextView textField = findViewById(R.id.nameFileField);
+
         Button button = findViewById(R.id.createButton);
         button.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                createfile(tempResource, "nomefile");
-                try {
-                    tempResource.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                if (textField.getText() != null) {
+                    String fileName = (String) textField.getText();
+                    createfile(tempResource, fileName);
+                    try {
+                        tempResource.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(getApplicationContext(), "File created", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getBaseContext(), Phone_Activity_Main.class);
+                    intent.putExtra("REFRESH", true);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Insert file name", Toast.LENGTH_SHORT).show();
                 }
-                Toast.makeText(getApplicationContext(),"Oggetto creato", Toast.LENGTH_SHORT);
+
             }
         });
     }
 
     private void createfile(InputStream iStream, String fileName){
-        //TODO aggiungere intent che richiama main e aggiorna il fragment di schedulelist
         File pathFile = new File(pathName);
         FileOutputStream oStream;
         fileName = pathName + File.separator + fileName + ".xml";
