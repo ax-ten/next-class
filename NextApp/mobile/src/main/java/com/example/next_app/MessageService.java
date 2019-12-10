b00b7982190f606821e3310a3aa950a1ca7c95c7
Axten <gigdamato@gmail.com>
2019-12-10 20:40:01 +0100

    Created new path and renamed existent one

--- NextApp/mobile/src/main/java/com/example/next_app/MessageService.java
+++ NextApp/mobile/src/main/java/com/example/next_app/MessageService.java
@@ -1,9 +1,5 @@
 package com.example.next_app;
 
-//TODO ci sono 2 messaggi da interpretare
-// -utente presente
-// -richiesta quotidiana dell'orario
-
 import android.content.Intent;
 
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
@@ -12,22 +8,31 @@ import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.WearableListenerService;
 
 public class MessageService extends WearableListenerService {
-    String path = "/my_path";
-    String payloadName = "stubArrayList";
+    Intent messageIntent;
 
+    String attendancePath = "/attendance";
+    String payloadName = "attendance";
+
+    String refreshPath = "/refreshSchedule";
     public MessageService() {
     }
 
     @Override
     public void onMessageReceived(MessageEvent messageEvent){
-        if (messageEvent.getPath().equals(path)){
+        if (messageEvent.getPath().equals(attendancePath)){
+
             final String message = new String(messageEvent.getData());
-            Intent messageIntent = new Intent();
+            messageIntent= new Intent();
             messageIntent.setAction(Intent.ACTION_SEND);
             messageIntent.putExtra(payloadName, message);
 
             LocalBroadcastManager.getInstance(this).sendBroadcast(messageIntent);
-        } else {
+
+
+        } else if (messageEvent.getPath().equals(refreshPath)) {
+            //TODO : implementare richiesta di aggiornamento
+
+        } else{
             super.onMessageReceived(messageEvent);
         }
     }
