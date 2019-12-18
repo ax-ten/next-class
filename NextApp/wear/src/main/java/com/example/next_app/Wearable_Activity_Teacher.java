a12f080a80a9c04ac12e55d86f3c3651cf9faebf
Axten <gigdamato@gmail.com>
2019-12-18 01:51:19 +0100

    Teacher Activity now loads layout

--- NextApp/wear/src/main/java/com/example/next_app/Wearable_Activity_Teacher.java
+++ NextApp/wear/src/main/java/com/example/next_app/Wearable_Activity_Teacher.java
@@ -1,7 +1,16 @@
 package com.example.next_app;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Wearable_Activity_Teacher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //updatePinPosition();
+        setContentView(R.layout.wearable_activity_teacher);
+    }
 
 }
