d37f322bac63ef32ea1db5d466648bb377ab93fb
Gigi <krillaxten@gmail.com>
2020-01-03 22:54:02 +0100

    Fixed Teacher activity

--- NextApp/wear/src/main/java/com/example/next_app/Wearable_Activity_Teacher.java
+++ NextApp/wear/src/main/java/com/example/next_app/Wearable_Activity_Teacher.java
@@ -2,18 +2,7 @@ package com.example.next_app;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.FragmentActivity;
-
 import android.support.wearable.activity.WearableActivity;
-import android.view.View;
-
-import java.util.Objects;
-
 public class Wearable_Activity_Teacher extends WearableActivity {
 
     @Override
